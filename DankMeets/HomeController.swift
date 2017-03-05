aa3e537efa22f24cba8100798f2b2dbb2b11e572
tchia007 <tchia007@ucr.edu>
2017-03-04 18:23:50 -0800

    added mapkit

--- DankMeets/HomeController.swift
+++ DankMeets/HomeController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class HomeController: UICollectionViewController, MKMapViewDelegate, UICollectionViewDelegateFlowLayout, CLLocationManagerDelegate {
+    
+    var window : UIWindow?
+    var mapView : MKMapView?
+    
+    var locationManager: CLLocationManager?
+    //The range (meter) of how much we want to see arround the user's location
+    let distanceSpan: Double = 500
 	
 	let cellId = "cellId"
 	
@@ -17,7 +25,33 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
 		
 		setupCollectionView()
 		setupMenuBar()
+        
+        //setting up map
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.view.backgroundColor = UIColor.white
+        
+        self.mapView = MKMapView(frame: CGRect(x:0, y:20, width: view.frame.width, height: view.frame.width))
+        self.view.addSubview(self.mapView!)
+        
+        //getting permissions to display your location on the map
+        self.locationManager = CLLocationManager()
+        if let locationManager = self.locationManager {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager.requestAlwaysAuthorization()
+            locationManager.distanceFilter = 50
+            locationManager.startUpdatingLocation()
+        }
+        
 	}
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, self.distanceSpan, self.distanceSpan)
+            mapView.setRegion(region, animated: true)
+            mapView.showsUserLocation = true
+        }
+    }
 	
 	let menuBar : MenuBar = {
 		let mb = MenuBar()
